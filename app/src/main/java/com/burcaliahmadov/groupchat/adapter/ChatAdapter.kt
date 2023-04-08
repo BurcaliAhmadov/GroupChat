422422436b75dab4275e139c68946fd1af2d3301
Burcali Ahmadov <burcaliahmadov97@gmail.com>
2023-04-08 11:10:37 +0400

    ChatFragment commited

---
+++ app/src/main/java/com/burcaliahmadov/groupchat/adapter/ChatAdapter.kt
@@ -0,0 +1,34 @@
+package com.burcaliahmadov.groupchat.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.burcaliahmadov.groupchat.databinding.ChatUserItemBinding
+import com.burcaliahmadov.groupchat.model.UserModel
+
+class ChatAdapter(var context: Context,var list:ArrayList<UserModel>): RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+
+    inner class  ChatViewHolder(val binding:ChatUserItemBinding) : ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val binding=ChatUserItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ChatViewHolder(binding)
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        val user=list[position]
+        Glide.with(context).load(user.imgUrl).into(holder.binding.userImage)
+        holder.binding.username.text=user.name
+    }
+}
